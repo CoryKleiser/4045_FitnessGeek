b49145812a0aacc5f8557290a9fa044d3c9c8e4b
Cory Kleiser <kleisecj@mail.uc.edu>
2016-10-08 00:09:29 -0400

    added User.java

---
+++ src/com/FitnessGeek/dto/User.java
@@ -0,0 +1,51 @@
+package com.FitnessGeek.dto;
+
+public class User {
+
+	private int userId;
+	private String userName, userPassword, firstName, lastName;
+	private int userWeight, userHeight;
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public int getUserHeight() {
+		return userHeight;
+	}
+	public void setUserHeight(int userHeight) {
+		this.userHeight = userHeight;
+	}
+	public int getUserWeight() {
+		return userWeight;
+	}
+	public void setUserWeight(int userWeight) {
+		this.userWeight = userWeight;
+	}
+	
+}
