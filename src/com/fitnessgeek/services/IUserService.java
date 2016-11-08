008fb8fd1dd9f32503f24f30466d331d06468de6
Cory Kleiser <kleisecj@mail.uc.edu>
2016-11-07 20:13:12 -0500

    updated UserService to handle activities

--- src/com/fitnessgeek/services/IUserService.java
+++ src/com/fitnessgeek/services/IUserService.java
@@ -2,6 +2,7 @@ package com.fitnessgeek.services;
 
 import java.util.Set;
 
+import com.fitnessgeek.dto.Activity;
 import com.fitnessgeek.dto.User;
 
 public interface IUserService {
@@ -17,5 +18,9 @@ public interface IUserService {
 	 * @return set of all users
 	 */
 	Set<User> showAllUsers();
+
+	void add(Activity activity);
+
+	Set<Activity> showAllActivities();
 
 }
--- src/com/fitnessgeek/services/UserService.java
+++ src/com/fitnessgeek/services/UserService.java
@@ -5,7 +5,9 @@ import java.util.Set;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import com.fitnessgeek.dao.IActivityDAO;
 import com.fitnessgeek.dao.IUserDAO;
+import com.fitnessgeek.dto.Activity;
 import com.fitnessgeek.dto.User;
 
 @Named
@@ -14,6 +16,10 @@ public class UserService implements IUserService {
 	@Inject
 	private
 	IUserDAO userDAO;
+	
+	@Inject
+	private
+	IActivityDAO activityDAO;
 		
 	/* (non-Javadoc)
 	 * @see com.fitnessgeek.services.IUserService#add(com.fitnessgeek.dto.User)
@@ -29,12 +35,26 @@ public class UserService implements IUserService {
 	public Set<User> showAllUsers(){
 		return getUserDAO().fetchAllUsers();
 	}
-	
+	@Override
+	public void add(Activity activity) {
+		getActivityDAO().insert(activity);
+	}
+	@Override
+	public Set<Activity> showAllActivities(){
+		return getActivityDAO().fetchAllActivities();
+	}
 	public IUserDAO getUserDAO() {
 		return userDAO;
 	}
 
 	public void setUserDAO(IUserDAO userDAO) {
 		this.userDAO = userDAO;
+	}
+
+	public IActivityDAO getActivityDAO() {
+		return activityDAO;
+	}
+	public void setActivityDAO(IActivityDAO activityDAO) {
+		this.activityDAO = activityDAO;
 	}
 }
