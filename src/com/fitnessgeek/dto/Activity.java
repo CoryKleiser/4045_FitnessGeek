ea0f353eb20401ee74cf2ee21bed32a5fc321032
deansc <deansc@mail.uc.edu>
2016-11-22 21:36:32 -0500

    Update Activity.java
    Added Javadoc
    
    -Spencer

--- src/com/fitnessgeek/dto/Activity.java
+++ src/com/fitnessgeek/dto/Activity.java
@@ -7,6 +7,10 @@ import javax.inject.Named;
 
 import org.springframework.context.annotation.Scope;
 
+
+/**
+ * Our Activity class
+ */
 @Named
 @ManagedBean
 @Scope("session")
@@ -18,34 +22,82 @@ public class Activity {
 	private String completionDate;
 	private String totalTrackingAmount;
 	
+	/**
+	 * gets the date completed
+	 * @return Date datecompleted
+	 */
 	public Date getDateCompleted() {
 		return dateCompleted;
 	}
+	
+	/**
+	 * takes in a date, and sets the dateCompleted equal to that date
+	 * @param Date dateCompleted
+	 */
 	public void setDateCompleted(Date dateCompleted) {
 		this.dateCompleted = dateCompleted;
 	}
 	
+	/**
+	 * gets the total tracking ammount
+	 * @return String totalTrackingAmount
+	 */
 	public String getTotalTrackingAmount() {
 		return totalTrackingAmount;
 	}
+	
+	/**
+	 * takes in a String and sets the total tracking amount equal to that string
+	 * @param String totalTrackingAmount
+	 */
 	public void setTotalTrackingAmount(String totalTrackingAmount) {
 		this.totalTrackingAmount = totalTrackingAmount;
 	}
+	
+	/**
+	 * gets the type name
+	 * @return String typeName
+	 */
 	public String getTypeName() {
 		return typeName;
 	}
+	
+	/**
+	 * takes in a String and sets the typeName equal to that string
+	 * @param String typeName
+	 */
 	public void setTypeName(String typeName) {
 		this.typeName = typeName;
 	}
+	
+	/**
+	 * gets the user id
+	 * @return int userId
+	 */
 	public int getUserId() {
 		return userId;
 	}
+	
+	/**
+	 * takes in an int and sets the userId equal to that int
+	 * @param int userId
+	 */
 	public void setUserId(int userId) {
 		this.userId = userId;
 	}
+	
+	/**
+	 * gets the completion date
+	 * @return String completionDate
+	 */
 	public String getCompletionDate() {
 		return completionDate;
 	}
+	
+	/**
+	 * takes in a String and sets the completionDate equal to that String
+	 * @param String completionDate
+	 */
 	public void setCompletionDate(String completionDate) {
 		this.completionDate = completionDate;
 	}
