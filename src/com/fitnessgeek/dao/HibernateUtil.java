66542c2715d1d179e020da54600981cc434a4236
deansc <deansc@mail.uc.edu>
2016-11-22 20:54:24 -0500

    Update HibernateUtil.java

--- src/com/fitnessgeek/dao/HibernateUtil.java
+++ src/com/fitnessgeek/dao/HibernateUtil.java
@@ -3,10 +3,21 @@ package com.fitnessgeek.dao;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+
+/**
+ * This is our Hibernate Utility class
+ * This class contains all of the necessary methods to implement hibernation
+ */
 public class HibernateUtil {
 
     private static final SessionFactory sessionFactory = buildSessionFactory();
 
+    /**
+    * Creates the SessionFactory from hibernate.cfg.xml
+    * No input taken
+    * @return Returns a new Session Factory Configuration
+    * Throws exception
+    */
     private static SessionFactory buildSessionFactory() {
         try {
             // Create the SessionFactory from hibernate.cfg.xml
@@ -19,10 +30,18 @@ public class HibernateUtil {
         }
     }
 
+    /**
+     * Gets the Session Factory
+     * @return returns the SessionFactory
+     */
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
     }
     
+    /**
+     * Close caches and connection pools
+     * No return from this method
+     */
     public static void shutdown() {
     	// Close caches and connection pools
     	getSessionFactory().close();
