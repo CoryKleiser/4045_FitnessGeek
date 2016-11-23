c718e176f6dfe9b1abf6b5d26beec038c32e1671
deansc <deansc@mail.uc.edu>
2016-11-22 21:44:19 -0500

    Update ActivityHistory.java
    Added Javadoc
    
    -Spencer

--- src/com/fitnessgeek/dto/ActivityHistory.java
+++ src/com/fitnessgeek/dto/ActivityHistory.java
@@ -21,30 +21,66 @@ public class ActivityHistory {
 	private int activityFrequency;
 	private List<Activity> activityCount;
 	
+	/**
+	 * gets the activity type
+	 * @return String activityType
+	 */
 	public String getActivityType() {
 		return activityType;
 	}
+	
+	/**
+	 * takes in a String and sets the activity type to that string
+	 * @param String activityType
+	 */
 	public void setActivityType(String activityType) {
 		this.activityType = activityType;
 	}
 	
+	/**
+	 * gets the activity id
+	 * @return int activityId
+	 */
 	public int getActivityId() {
 		return activityId;
 	}
+	
+	/**
+	 * takes in an int and sets the activityId to that int
+	 * @param int activityId
+	 */
 	public void setActivityId(int activityId) {
 		this.activityId = activityId;
 	}
 	
+	/**
+	 * gets the activity frequency
+	 * @return int activityFrequency
+	 */
 	public int getActivityFrequency() {
 		return activityFrequency;
 	}
+	
+	/**
+	 * takes in an int and sets the activity frequency to that int
+	 * @param int activityFrequency
+	 */
 	public void setActivityFrequency(int activityFrequency) {
 		this.activityFrequency = activityFrequency;
 	}
 	
+	/**
+	 * gets the activity count
+	 * @return List<Activity> activityCount
+	 */
 	public List<Activity> getActivityCount() {
 		return activityCount;
 	}
+	
+	/**
+	 * takes in a List<Avtivity> and sets the activityCount equal to that List<Activity>
+	 * @param List<Avtivity> activityCount
+	 */
 	public void setActivityCount(List<Activity> activityCount) {
 		this.activityCount = activityCount;
 	}
