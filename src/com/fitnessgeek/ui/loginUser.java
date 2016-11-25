5252ba55dd97ad77fe75264ea4eccd1585e8e894
deansc <deansc@mail.uc.edu>
2016-11-25 00:01:37 -0800

    Update loginUser.java
    Added Javadoc
    
    -Spencer

--- src/com/fitnessgeek/ui/loginUser.java
+++ src/com/fitnessgeek/ui/loginUser.java
@@ -14,26 +14,51 @@ public class loginUser {
 
 	public User user;
 
+	/**
+	 * Gets the user
+	 * @return User
+	 */
 	public User getUser() {
 		return user;
 	}
-
+	
+	/**
+	 * This method takes in a User and sets the current user equal to that value
+	 * @param User user
+	 */
 	public void setUser(User user) {
 		this.user = user;
 	}
 	
+	/**
+	 * This method attemps to log a user in given a username and a password
+	 * @param String username, String password
+	 * @return String
+	 */
 	public String execute(String username, String password) {
 		return "success";
 	}
 	
+	/**
+	 * This method is called when a user tries to register
+	 * @return String
+	 */
 	public String register() {
 		return "success";
 	}
 	
+	/**
+	 * This method is called when a user tries to login via Facebook authentication
+	 * @return String
+	 */
 	public String facebookLogin() {
 		return "success";
 	}
 	
+	/**
+	 * This method is called when a user tries to login via Google authentication
+	 * @return String
+	 */
 	public String googleLogin() {
 		return "success";
 	}
